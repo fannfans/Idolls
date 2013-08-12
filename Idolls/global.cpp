22b2520a1a2bf9a390f5c9c97f0e9be11013916d
HyoeunKim <Hyonnnnn@gmail.com>
2013-08-13 00:36:11 +0900

    전역변수 파일 global.cpp / global.h 추가

---
+++ Idolls/global.cpp
@@ -0,0 +1,9 @@
+//
+//  global.cpp
+//  Idolls
+//
+//  Created by 효은 김 on 13. 8. 13..
+//
+//
+
+#include "global.h"
---
+++ Idolls/global.h
@@ -0,0 +1,14 @@
+//
+//  global.h
+//  Idolls
+//
+//  Created by 효은 김 on 13. 8. 13..
+//
+//
+
+#ifndef __Idolls__global__
+#define __Idolls__global__
+
+#include <iostream>
+
+#endif /* defined(__Idolls__global__) */
