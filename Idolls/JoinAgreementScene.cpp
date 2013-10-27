ed6f1d70a2e8fb81b6ab0bdce08824c7069237be
박성수 <jasontody@gmail.com>
2013-10-27 13:42:56 +0900

    Bob Image Modify, Title / Logo 수정 , 헤더 메뉴 수정, 하단 copyright 및 social 부분 수정

---
+++ Idolls/JoinAgreementScene.cpp
@@ -0,0 +1,9 @@
+//
+//  JoinAgreementScene.cpp
+//  Idolls
+//
+//  Created by 박성수 on 13. 9. 18..
+//
+//
+
+#include "JoinAgreementScene.h"
---
+++ Idolls/JoinAgreementScene.h
@@ -0,0 +1,14 @@
+//
+//  JoinAgreementScene.h
+//  Idolls
+//
+//  Created by 박성수 on 13. 9. 18..
+//
+//
+
+#ifndef Idolls_JoinAgreementScene_h
+#define Idolls_JoinAgreementScene_h
+
+
+
+#endif
